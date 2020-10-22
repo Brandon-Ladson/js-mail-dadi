fa1e8de28648b152e0943b3d0a3c3b81e63a8628
Brandon Ladson <brandonladson1999@gmail.com>
2020-10-22 14:36:11 +0200

    creazione regole js (da rifinire)

---
+++ gioco-dadi/js/script.js
@@ -0,0 +1,31 @@
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// numero dell'utente
+var lancioUtente;
+
+// numero del computer
+var lancioCumputer;
+
+// premere bottone per lanciare il dado
+var bottone = document.getElementById('bottonelancio');
+
+bottone.addEventListener('click',
+ function() {
+
+   lancioUtente = Math.floor(Math.random() * 6) + 1;
+
+   lancioCumputer = Math.floor(Math.random() * 6) + 1;
+
+   if (lancioUtente > lancioCumputer) {
+     document.getElementById('risultato').innerHTML = "HAI VINTO!"
+
+   } else if (lancioCumputer > lancioUtente) {
+     document.getElementById('risultato').innerHTML = "HAI PERSO!"
+
+   } else {
+     document.getElementById('risultato').innerHTML = "PAREGGIO!"
+   }
+
+} );
+// definire il vincitore
